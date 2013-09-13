7adeef7465b87afc71c3b489bce4ad9d4fa42409
Kelly Caylor <kcaylor@princeton.edu>
2013-09-13 17:25:34 -0400

    Added port handling in evepod.py

--- evepod.py
+++ evepod.py
@@ -4,6 +4,16 @@ from eve import Eve
 
 app = Eve(settings='settings.py')
 
+# Heroku defines a $PORT environment variable that we use to determine
+# if we're running locally or not.
+port = os.environ.get('PORT')
+if port:
+    host = '0.0.0.0'
+    port = int(port)
+else:
+    host = '127.0.0.1'
+    port = 5000
+
 # Start the application
-#if __name__ == '__main__':
-app.run(debug=False)
+if __name__ == '__main__':
+    app.run(host=host, port=port)
